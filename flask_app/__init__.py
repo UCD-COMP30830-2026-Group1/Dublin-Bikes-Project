e2d561a1fc826a09739f349c59128f9c4db73145
J-YuhanWang <wangyuhan6459@gmail.com>
2026-03-02 11:06:24 +0000

    feat(historical): migrate data source from JSON to RDS with session management and modularize station related logic into station_routes.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from flask_app.routes.station_routes import realtime_bp
+from flask_app.routes.station_routes import station_bp
 from flask_app.routes.weather_routes import weather_bp
 
 
@@ -8,8 +8,8 @@ def create_app():
     app = Flask(__name__)
 
     # Import routes
-    app.register_blueprint(realtime_bp)
-
     app.register_blueprint(weather_bp)
+
+    app.register_blueprint(station_bp)
 
     return app
