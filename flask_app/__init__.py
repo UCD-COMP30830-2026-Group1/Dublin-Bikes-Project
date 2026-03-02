6c751aff79d8952bf5800200a46de4a186b33edc
J-YuhanWang <wangyuhan6459@gmail.com>
2026-03-02 19:14:48 +0000

    feat: enable CORS to support frontend cross-origin requests

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from flask_app.routes.station_routes import station_bp
 from flask_app.routes.weather_routes import weather_bp
@@ -6,6 +7,7 @@ from flask_app.routes.weather_routes import weather_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     # Import routes
     app.register_blueprint(weather_bp)
