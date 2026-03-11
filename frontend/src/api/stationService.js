2bae007d6312187dd938c747d3ac865e92c9beef
J-YuhanWang <wangyuhan6459@gmail.com>
2026-03-11 11:23:14 +0000

    Refactoring (Map): Initialize MapView and load the map to be used as the infrastructure, completing the function of retrieving stations.

--- frontend/src/api/stationService.js
+++ frontend/src/api/stationService.js
@@ -1,7 +1,17 @@
 // src/api/stationService.js
-const API_BASE_URL = 'https://bikes.thegaff.io/api';
+const API_DOMAIN = import.meta.env.VITE_API_DOMAIN || 'localhost:5000';
+const protocol = API_DOMAIN.includes('localhost') ? 'http' : 'https';
+const API_BASE_URL = `${protocol}://${API_DOMAIN}/api`;
 
 export async function fetchStaticStations() {
-    const response = await fetch(`${API_BASE_URL}/stations/static`);
+    const targetUrl = `${API_BASE_URL}/stations/static`;
+    console.log("Actual request url:", targetUrl);
+
+    const response = await fetch(targetUrl);
+
+    if (!response.ok) {
+        throw new Error(`Request failed. Error: ${response.status}`);
+    }
+
     return await response.json();
 }
